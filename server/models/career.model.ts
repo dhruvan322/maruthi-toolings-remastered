896b4b69bac09d0a800fe630f0a1ad38cdd57daf
V.Dhruvan Reddy <dhruvanreddy3311@gmail.com>
2025-10-30 01:34:57 +0530

    Update career.model.ts

--- server/models/career.model.ts
+++ server/models/career.model.ts
@@ -1,10 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
-import { CareerPost } from '../../types';
+import { CareerPost } from '../../types.js'; // FIX: added .js extension
 
 const careerSchema: Schema = new Schema({
-    position: { type: String, required: true },
-    description: { type: String, required: true },
-    location: { type: String, required: true },
+  position: { type: String, required: true },
+  description: { type: String, required: true },
+  location: { type: String, required: true },
 });
 
 export default mongoose.model<CareerPost>('Career', careerSchema);
