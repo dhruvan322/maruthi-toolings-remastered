94f547b57f8d1bfb0c622ee565dd1e16d057f12f
V.Dhruvan Reddy <dhruvanreddy3311@gmail.com>
2025-10-30 03:06:23 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -48,8 +48,5 @@ const startServer = async () => {
     process.exit(1);
   }
 };
-app.get('/', (req, res) => {
-  res.send('✅ Maruthi Toolings Backend is running successfully!');
-});
 
 startServer();
