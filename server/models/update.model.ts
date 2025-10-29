d5090b2f1c7754502c2f5f75642c476093f8826d
V.Dhruvan Reddy <dhruvanreddy3311@gmail.com>
2025-10-30 01:36:28 +0530

    Update update.model.ts

--- server/models/update.model.ts
+++ server/models/update.model.ts
@@ -1,10 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
-import { Update } from '../../types';
+import { Update } from '../../types.js'; // FIX: added .js extension
 
 const updateSchema: Schema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    date: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: String, required: true },
 });
 
 export default mongoose.model<Update>('Update', updateSchema);
