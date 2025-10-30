0e8e08acc85154e8411c050b006a181d7f6b35d6
V.Dhruvan Reddy <dhruvanreddy3311@gmail.com>
2025-10-30 19:12:00 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -7,8 +7,8 @@ export const NAV_LINKS_LEFT: Page[] = ['Products', 'End Components', 'Tools & Ma
 export const NAV_LINKS_RIGHT: Page[] = ['Services', 'About Us', 'Contact Us', 'Careers'];
 
 export const BANNER_IMAGES = [
-  'https://picsum.photos/seed/banner1/1920/1080',
-  'https://picsum.photos/seed/banner2/1920/1080',
-  'https://picsum.photos/seed/banner3/1920/1080',
-  'https://picsum.photos/seed/banner4/1920/1080',
+  'https://www.shutterstock.com/image-photo/closeup-cnc-milling-machine-during-260nw-2482049639.jpg',
+  'https://media.gettyimages.com/id/529114252/photo/plastic-production-line.jpg?s=612x612&w=gi&k=20&c=B-8_uM1LoiYsI789JafSeTJ2WHDEqRhPvywKJAw04Hc=',
+  'https://media.istockphoto.com/id/1132975668/photo/row-of-molds-in-shelves-in-factory.jpg?s=612x612&w=0&k=20&c=WakdHAe6L-JowgAF_ftVOFd4Gw9ih68l51BdTzro63M=',
+  'https://5.imimg.com/data5/SELLER/Default/2024/5/417020710/FO/HN/NB/2035221/plastic-injection-molding-500x500.jpg',
 ];
